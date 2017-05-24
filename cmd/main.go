e6be9441513eff13ff5e103341639cc26d4cd18d
Radek Markiewicz <rmarkiew@gmail.com>
2017-05-25 00:26:16 +0200

    params handling + cleaning

--- cmd/main.go
+++ cmd/main.go
@@ -7,23 +7,18 @@ import (
 	"sync"
 
 	"github.com/majdanrc/cluster"
-	"github.com/majdanrc/cluster/log"
 )
 
 func main() {
 	file := flag.String("file", "", "file of chat logs, each line is a chat log, include timestamp, message ID, sender and content.")
-	max := flag.Int("max", 0, "[optional] max allowed cluster number within a day (24 hours). This parameter allow to control the generated number of cluster within a 24 hours.")
-	par := flag.Int("par", 0, "[optional] number of workers, default 4")
+	max := flag.Int("max", 0, "[optional] max allowed cluster number within a day (24 hours) - default 24")
+	par := flag.Int("par", 0, "[optional] number of workers - default 4")
 	flag.Parse()
 
 	if *file == "" {
 		flag.Usage()
 		return
 	}
-
-	log.Info("cluster.parameters.file", "%s", *file)
-	log.Info("cluster.parameters.max", "%d", *max)
-	log.Info("cluster.parameters.par", "%d", *par)
 
 	var maxClusters int
 	if *max > 0 {
@@ -32,9 +27,16 @@ func main() {
 		maxClusters = 24
 	}
 
+	var workers int
+	if *max > 0 {
+		workers = *par
+	} else {
+		workers = 4
+	}
+
 	chat, err := os.Open(*file)
 	if err != nil {
-		log.Error("error.file", err.Error())
+		fmt.Printf("%s", err.Error())
 		return
 	}
 
@@ -45,7 +47,7 @@ func main() {
 
 	input := cluster.NewReader(chat).Read()
 
-	for ind := 0; ind <= 20; ind++ {
+	for ind := 0; ind <= workers; ind++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
